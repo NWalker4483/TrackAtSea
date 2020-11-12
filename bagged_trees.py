67ebc010d5b0ac1db5cd790e50aae9609b56caa5
Nile Walker <niwal7@morgan.edu>
2020-11-12 14:40:00 -0500

    Readded Bagged Regressor

--- bagged_trees.py
+++ bagged_trees.py
@@ -1,4 +1,3 @@
-from sklearn.svm import SVR
 from sklearn.ensemble import BaggingRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
@@ -6,8 +5,8 @@ from utils.common import load
 import numpy as np
 import geopy.distance
 
-fit_videos = [7,16,13,14]
-test_videos = [7,16,13,14] #[14,12,10,21]
+fit_videos = [7,13,16,21]
+test_videos = [14,12,10,21]
 
 distorted_points, gps_points = load(fit_videos[0])
 for i in range(1,len(fit_videos)):
@@ -21,13 +20,10 @@ for i in range(1,len(fit_videos)):
     distorted_points_test = np.concatenate((distorted_points_test, distorted_points_temp), axis=0)
     gps_points_test = np.concatenate((gps_points_test, gps_points_temp), axis=0)
 
-lat_regressor = DecisionTreeRegressor(random_state=0)
-lon_regressor = DecisionTreeRegressor(random_state=0)
-lat_regressor.fit(distorted_points, gps_points[:,0])
-lon_regressor.fit(distorted_points, gps_points[:,1])
-
-# cross_val_score(lat_regressor, distorted_points, gps_points[:,0], cv=10) # doctest: +SKIP
-# cross_val_score(lon_regressor, distorted_points, gps_points[:,1], cv=10) # doctest: +SKIP
+lat_regressor = BaggingRegressor(base_estimator=DecisionTreeRegressor(),
+                        n_estimators=10, random_state=0).fit(distorted_points, gps_points[:,0])
+lon_regressor = BaggingRegressor(base_estimator=DecisionTreeRegressor(),
+                        n_estimators=10, random_state=0).fit(distorted_points, gps_points[:,1])
 
 gps_projections = list(zip(lat_regressor.predict(distorted_points_test), lon_regressor.predict(distorted_points_test)))
 # Compute Projection Distance Error
@@ -35,5 +31,5 @@ ERR = []
 for i in range(len(gps_projections)):
     ERR.append(geopy.distance.geodesic(gps_projections[i], gps_points_test[i]).m)
 print(f"Average Error (m) {np.mean(ERR)}")
-print(f"Average Error (m) {np.median(ERR)}")
+print(f"Median Error (m) {np.median(ERR)}")
 print(f"Error Standard Deviation (+/- m) {np.std(ERR)}")
