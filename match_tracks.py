ec850e3bbdec41b65a8fb6b3249abf81067f6249
Nile Walker <niwal7@morgan.edu>
2020-12-01 16:34:05 -0500

    Deleted Dataset

--- match_tracks.py
+++ match_tracks.py
@@ -1,33 +1,9 @@
 from trackers.DEEP import DEEPTracker
-from trackers.MANUAL import ManualTracker
 import argparse
 import random
 import pickle 
 import cv2
 import csv 
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--video_num', type=int, required=True)
-
-parser.add_argument('--video_file', type=str, default="raw_data/video/7.mp4",
-                    help='Video file name (MP4 format)')
-
-args = parser.parse_args()
-
-cap = cv2.VideoCapture(args.video_file)
-         
-det_frames = dict()
-with open(f"generated_data/tracks/orb.{args.video_num}.csv","r") as f:
-    content = f.readlines()
-    content = content[1:] # Skip Header
-for entry in content:
-    Frame_No, ID, X1, Y1, X2, Y2 = [int(i) for i in entry.split(",")]
-    if int(Frame_No) in det_frames:
-        det_frames[int(Frame_No)].append((ID, [X1, Y1, X2, Y2]))
-    else:
-        det_frames[int(Frame_No)] = [(ID,[X1, Y1, X2, Y2])]
-def match_tracks(tracks):
-    return tracks
 
 def scaleROI(box, scale = 2):
     X1, Y1, X2, Y2 = box 
@@ -37,37 +13,35 @@ def scaleROI(box, scale = 2):
     NY2 = Y2 + (Y2 - Y1) * (scale - 1)/ 2
     return [int(NX1), int(NY1), int(NX2), int(NY2)]
 
-matched = set()
-scores = dict()
-scale = 4
-thresh = 3
-frame_num = 0
-tracker = DEEPTracker()
-while True:
-    ret, frame = cap.read()
-    if not ret: break
-    if frame_num in det_frames:
-        for ID, box in det_frames[frame_num]:
-            if ID in matched: continue 
-            box = scaleROI(box, scale)
-            cv2.imshow("Detecting",frame[box[1]:box[3], box[0]:box[2]])
-            cv2.waitKey(1)
-            detections = tracker.update(frame[box[1]:box[3], box[0]:box[2]]) # Scale and Crop
-            if len(detections) != 0:
-                scores[ID] = scores[ID] + 1 if ID in scores else 1
-                if scores[ID] >= thresh:
-                    matched.add(ID)
-    frame_num += 1
-print(matched)
-with open(f"generated_data/tracks/orb.matched.{args.video_num}.csv","w+") as out:
-    fields = ['Frame No.', 'Vessel ID','X1','Y1','X2','Y2']  
-    csvwriter = csv.writer(out)  
-    csvwriter.writerow(fields)  
+def match_tracks(detections, video_name, thresh = 3, scale = 4):
+    matched = set()
+    scores = dict()
+    frame_num = 0
+    tracker = DEEPTracker()
+    cap = cv2.VideoCapture(video_name)
+    frame_num = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret: break
+        if frame_num in detections:
+            for ID in detections[frame_num]:
+                if ID in matched: continue 
+                box = scaleROI(detections[frame_num][ID], scale)
+                det = tracker.update(frame[box[1]:box[3], box[0]:box[2]]) # Scale and Crop
+                if len(det) != 0:
+                    scores[ID] = scores[ID] + 1 if ID in scores else 1
+                    if scores[ID] >= thresh:
+                        matched.add(ID)
+        frame_num += 1
+    tracks = dict()
+    for frame_num in detections:
+        for ID in detections[frame_num]:
+            if ID not in tracks: tracks[ID] = dict()
+            if ID in matched:
+                tracks[-1][frame_num] = detections[frame_num][ID]
+            else:
+                tracks[ID][frame_num] = detections[frame_num][ID]
+    return tracks
 
-    for entry in content:
-        Frame_No, ID, X1, Y1, X2, Y2 = [int(i) for i in entry.split(",")]
-        if int(ID) in matched:
-            csvwriter.writerow([Frame_No, -1, X1, Y1, X2, Y2])
-        else:
-            csvwriter.writerow([Frame_No, ID, X1, Y1, X2, Y2])
 
+         
--- tool.py
+++ tool.py
@@ -6,9 +6,9 @@ import csv
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
-parser.add_argument('--video_num', type=int, default=None)
+parser.add_argument('--video_num', type=int, default=6)
 
-parser.add_argument('--video_file', type=str, default="raw_data/video/6.mp4",
+parser.add_argument('--video_file', type=str,
                     help='Video file name (MP4 format)')
 parser.add_argument('--num_detections', type=int, default=15,
                     help='')
@@ -22,16 +22,14 @@ parser.add_argument('--tracker_type', type=str, default="ORB",
                     help='')
 
 args = parser.parse_args()
-if args.video_num != None:
+if args.video_file == None:
     args.video_file = f"raw_data/video/{args.video_num}.mp4"
     
 video = cv2.VideoCapture(args.video_file)
 frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-frames_read = 0
 
 if args.tracker_type == "ORB":
     from trackers.ORB import ORBTracker
-    Land = 420 
     tracker = ORBTracker(Land_at=420)
 
 # elif args.tracker_type == "SORT":
@@ -60,7 +58,7 @@ else:
     print("No tracker of the name exists")
     raise(KeyError)
 
-tracks = dict()
+detections = dict()
 print("Detecting...")
 while video.isOpened():
     ret, frame = video.read()
@@ -69,33 +67,38 @@ while video.isOpened():
     det = tracker.update(frame)
     if len(det) > 0:
         for frame_num, ID, box in det:
-            ID = -1 
-            if ID not in tracks: tracks[ID] = dict()
-            tracks[ID][frame_num] = box
+            if frame_num not in detections: detections[frame_num] = dict()
+            detections[frame_num][ID] = box
 # Determine Landmark Track
-#from match_tracks import match_tracks
+from match_tracks import match_tracks
 print("Matching...")
-#tracks = match_tracks(tracks)
+tracks = match_tracks(detections, args.video_file)
 # Load Homography and Camera Coefficients 
 params = pickle.load(open("generated_data/best_camera_params.pkl","rb"))
 # Generate and save results 
-out_file = open(f"generated_data/outputs/{args.video_num}.{args.tracker_type.lower()}.csv","w+")
+out_file = open(f"outputs/{args.video_num}.{args.tracker_type.lower()}.csv","w+")
 fields = ['Frame No.', 'Vessel ID', 'Latitude', 'Longitude', 'X', 'Y']  
 csvwriter = csv.writer(out_file)  
 csvwriter.writerow(fields)  
 try:
+    print("Projecting...")
     for ID in tracks:
         distorted_points = []
+        frames = []
         for frame_num in tracks[ID]:
             X1, Y1, X2, Y2 = tracks[ID][frame_num]
             X, Y = [X1+((X2 - X1)//2),Y2]
+            frames.append(frame_num)
             distorted_points.append([[X],[Y]])
 
         undistorted_points = cv2.undistortPoints(
-                np.array(distorted_points), params["Intrinsic Matrix"], params["Distortion Coefficients"], P=params["Intrinsic Matrix"])
+                np.array(distorted_points, dtype=np.float64), params["Intrinsic Matrix"], params["Distortion Coefficients"], P=params["Intrinsic Matrix"])
 
         gps_projections = cv2.perspectiveTransform(undistorted_points, params["Homography"])
-        print(gps_projections)
-        #csvwriter.writerow( [frames_read, 0, lat, lon, x, y])
+        gps_projections = gps_projections.reshape(-1, 2)
+        for i in range(len(frames)):
+            lat, lon = gps_projections[i]
+            x, y = distorted_points[i][0][0], distorted_points[i][1][0]
+            csvwriter.writerow([frames[i], ID, lat, lon, x, y])
 finally:
     out_file.close()
